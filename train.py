51fdf8e4f102bcdae179ce214eb46083817b9ff9
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2023-11-23 17:58:03 -0500

    add get_batch

--- train.py
+++ train.py
@@ -17,15 +17,23 @@ first_90_percent = int(len(data) * 0.9)
 train_data = data[:first_90_percent]
 val_data = data[first_90_percent:]
 
+batch_size = 4
 block_size = 8
 
 x = train_data[:block_size]
 y = train_data[1:block_size+1]
 
-print (x)
-print (y)
+# print (x)
+# print (y)
 
-for t in range(block_size): 
-    context = x[:t+1]
-    target = y[t]
-    print(f"t={t} context={decode(context.tolist())} target={decode([target.item()])}")
+# for t in range(block_size): 
+#     context = x[:t+1]
+#     target = y[t]
+#     print(f"t={t} context={decode(context.tolist())} target={decode([target.item()])}")
+
+def get_batch():
+    data = train_data
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    print (ix)
+
+get_batch()
