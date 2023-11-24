65cd5efd92be9cfa6f407d218e0b9709cab972c1
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2023-11-24 00:21:49 -0500

    better init

--- train.py
+++ train.py
@@ -116,6 +116,16 @@ class BigramLanguageModel(nn.Module):
         self.ln_f = nn.LayerNorm(num_embeddings) # final layer norm
         self.lm_head = nn.Linear(num_embeddings, vocab_size)
 
+        self.apply(self._init_weights)
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
     def forward(self, idx, targets=None):
 
         B, T = idx.shape
