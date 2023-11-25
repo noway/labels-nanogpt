3b104e0a29b1af39026179541a9dc2135f901f85
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2023-11-24 22:29:03 -0500

    generate_section_of_a_chapter

--- gendata.py
+++ gendata.py
@@ -1,3 +1,4 @@
+import json
 from openai import OpenAI
 import os
 from dotenv import load_dotenv
@@ -16,7 +17,16 @@ def ask_gpt4_chat(question):
     except Exception as e:
         return str(e)
 
-if __name__ == "__main__":
-    question = "Generate Kindergarten level K-12 math problem. No emojis. Only output the question, no assistant text."
+def generate_section_of_a_chapter(level, chapter, section):
+    question = f'You\'re writing a math textbook for {level} level K-12. Write the "{section}" section of the "{chapter}" chapter. Make sure the section is thorough and complete, including example excercises and answeres. Don\'t use emojis.'
     answer = ask_gpt4_chat(question)
-    print("GPT-4 says:", answer)
+    return answer
+
+level = 'Kindergarten'
+chapter = 'Counting from 1 to 10'
+section = 'Introduction to Numbers'
+text = generate_section_of_a_chapter('Kindergarten', 'Counting from 1 to 10', 'Introduction to Numbers')
+print(text)
+filename = f'{level}_{chapter}_{section}.txt'
+with open(filename, 'w') as f:
+    f.write(text)
