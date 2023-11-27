c507d48433c1ece7d2e8d309fbba33af42729903
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2023-11-27 01:30:39 -0500

    don't wrap normalized md output

--- md2md.sh
+++ md2md.sh
@@ -5,5 +5,5 @@ OUTPUT_DIR="dataset_md2md"
 [ ! -d "$OUTPUT_DIR" ] && mkdir -p "$OUTPUT_DIR"
 for file in "$SOURCE_DIR"/*.txt; do
     filename=$(basename "$file")
-    pandoc -f markdown -t markdown -o "$OUTPUT_DIR/$filename" "$file"
+    pandoc --wrap none -f markdown -t markdown -o "$OUTPUT_DIR/$filename" "$file"
 done
