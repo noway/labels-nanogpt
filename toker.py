a57da8ed9cd276493542b0daca2a13ea780bad91
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2023-11-29 03:40:53 -0500

    implement bpe-like tokenization for syllables

--- toker.py
+++ toker.py
@@ -310,7 +310,7 @@ for token, count in most_common_tokens:
             all_syllables[syllable] = 0
         all_syllables[syllable] += count
 
-# print (all_syllables)
+print (all_syllables)
 # all_syllables_items = all_syllables
 # print (all_syllables_items)
 # sorted_all_syllables_items = sorted(all_syllables_items, key=lambda x: len(x[0]), reverse=True)
@@ -330,7 +330,8 @@ splits = {
 def compute_pair_scores(splits):
     letter_freqs = defaultdict(int)
     pair_freqs = defaultdict(int)
-    for word, freq in all_syllables.items():
+    for word, _freq in all_syllables.items():
+        freq = 1 # every word has a weight of 1 - this is divergent from wordpiece/bpe
         split = splits[word]
         if len(split) == 1:
             letter_freqs[split[0]] += freq
@@ -341,14 +342,17 @@ def compute_pair_scores(splits):
             pair_freqs[pair] += freq
         letter_freqs[split[-1]] += freq
     scores = {
-        pair: freq / (letter_freqs[pair[0]] * letter_freqs[pair[1]])
+        # wordpiece optimizes for smallest vocab. this is just compute efficiency. works ok for translation.
+        # bpe optimizes for most frequent pairs, which is closer to human learning.
+        # whatever we see most, we memorize.
+        pair: freq # / (letter_freqs[pair[0]] * letter_freqs[pair[1]])
         for pair, freq in pair_freqs.items()
     }
     return scores
 
 pair_scores = compute_pair_scores(splits)
-for i, key in enumerate(pair_scores.keys()):
-    print(f"{key}: {pair_scores[key]}")
+# for i, key in enumerate(pair_scores.keys()):
+#     print(f"{key}: {pair_scores[key]}")
 #     if i >= 5:
 #         break
 
@@ -360,6 +364,48 @@ for pair, score in pair_scores.items():
         max_score = score
 
 print("best", best_pair, max_score)
+
+def merge_pair(a, b, splits):
+    for word in all_syllables:
+        split = splits[word]
+        if len(split) == 1:
+            continue
+        i = 0
+        while i < len(split) - 1:
+            if split[i] == a and split[i + 1] == b:
+                merge = a + b[2:] if b.startswith("##") else a + b
+                split = split[:i] + [merge] + split[i + 2 :]
+            else:
+                i += 1
+        splits[word] = split
+    return splits
+
+vocab = list()
+
+vocab_size = 70
+while len(vocab) < vocab_size:
+    scores = compute_pair_scores(splits)
+    best_pair, max_score = "", None
+    for pair, score in scores.items():
+        if max_score is None or max_score < score:
+            best_pair = pair
+            max_score = score
+    splits = merge_pair(*best_pair, splits)
+    # print(splits)
+    new_token = (
+        best_pair[0] + best_pair[1][2:]
+        if best_pair[1].startswith("##")
+        else best_pair[0] + best_pair[1]
+    )
+    vocab.append(new_token)
+
+print(splits)
+print(vocab)
+
+# splits = merge_pair(best_pair[0], best_pair[1], splits)
+# print(splits)
+# splits["about"]
+
 
 # print(compute_pair_scores(splits))
 # print(sorted_all_syllables_items)
