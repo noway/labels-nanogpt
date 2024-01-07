4060c09b0b2707df841f5ba08f524607bf21c9e1
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2024-01-07 14:32:40 -0500

    abstract out check_one_eval function

--- eval.py
+++ eval.py
@@ -15,38 +15,41 @@ with open('full_vocab.json', 'r') as f:
     full_vocab = eval(f.read())
 
 
-with open('exercises0_0.yml', 'r') as f:
-    data = yaml.safe_load(f)
-    text = data['exercises']
-    answer = data['answer']
+def check_one_eval(eval_file):
+    with open(eval_file, 'r') as f:
+        data = yaml.safe_load(f)
+        text = data['exercises']
+        answer = data['answer']
 
-print ('text',(text,))
-tokens = tokens_to_array_of_numbers_without_full_vocab(tokenize(text.lower(), splits, commonality_map), full_vocab)
-idx = torch.tensor(tokens).unsqueeze(0)
+    # print ('text',(text,))
+    tokens = tokens_to_array_of_numbers_without_full_vocab(tokenize(text.lower(), splits, commonality_map), full_vocab)
+    idx = torch.tensor(tokens).unsqueeze(0)
 
-print (idx.shape)
-print (decode(tokens), end='', flush=True)
+    # print (idx.shape)
+    # print (decode(tokens), end='', flush=True)
 
-device = (
-    'cuda'
-    if torch.cuda.is_available()
-    else 'mps'
-    if hasattr(torch.backends, 'mps') and torch.backends.mps.is_available()
-    else 'cpu'
-)
+    device = (
+        'cuda'
+        if torch.cuda.is_available()
+        else 'mps'
+        if hasattr(torch.backends, 'mps') and torch.backends.mps.is_available()
+        else 'cpu'
+    )
 
-idx = idx.to(device)
-the_answer = ''
-with torch.no_grad():
-    for token in m.module.generate(idx, 1000):
-        token_str = decode_one_token(token)
-        print(token_str, end='', flush=True)
-        if token_str == '\n':
-            break
-        the_answer += token_str
+    idx = idx.to(device)
+    the_answer = ''
+    with torch.no_grad():
+        for token in m.module.generate(idx, 1000):
+            token_str = decode_one_token(token)
+            # print(token_str, end='', flush=True)
+            if token_str == '\n':
+                break
+            the_answer += token_str
 
-print ('the_answer', (the_answer,))
+    print ('the_answer', (the_answer,))
+    is_correct = the_answer.strip() == str(answer)
+    print ('is_correct', (is_correct,))
 
-is_correct = the_answer.strip() == str(answer)
 
-print ('is_correct', (is_correct,))
+if __name__ == "__main__":
+    check_one_eval('exercises0_0.yml')
