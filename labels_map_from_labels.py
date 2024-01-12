71ab088f7131802318b675a4b18b2013ee37bdc8
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2024-01-11 21:22:15 -0500

    ruff format labels_map_from_labels.py

--- labels_map_from_labels.py
+++ labels_map_from_labels.py
@@ -6,8 +6,11 @@ with open('labels.json') as f:
 with open('labels_non_vectorized.json') as f:
     labels_non_vectorized = json.load(f)
 
-result = {label_non_vectorized: label for label, label_non_vectorized in zip(labels, labels_non_vectorized)}
-print (result)
+result = {
+    label_non_vectorized: label
+    for label, label_non_vectorized in zip(labels, labels_non_vectorized)
+}
+print(result)
 
 with open('../labels_map.json', 'w') as f:
     json.dump(result, f, indent=4)
