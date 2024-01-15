ee591249f9d0f342134140203a598d5c032c8a6d
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2024-01-14 21:01:40 -0500

    send notification to a webhook

--- track_lambda_changes.sh
+++ track_lambda_changes.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+source .env
 SOUND_FILE="/System/Library/Sounds/Funk.aiff"
 getCurrentOutput() {
     ./track_instance_types.sh | jq '.data[] | select(.regions_with_capacity_available != []).instance_type.name'
@@ -11,6 +12,8 @@ while true; do
     if [ "$current_output" != "$previous_output" ]; then
         afplay "$SOUND_FILE" &
         osascript -e 'display notification "Any capacity has changed" with title "Notification" subtitle "Please attend"'
+        current_output_processed="$(echo $current_output | tr -d '\"')"
+        curl -X POST -H "Content-Type: application/json" -d "{ \"username\": \"lambda-tracker\", \"content\": \"<@$DISCORD_USER_ID> capacity has changed: $current_output_processed\" }" $DISCORD_WEBHOOK_URL
     fi
     previous_output=$current_output
     sleep 1
