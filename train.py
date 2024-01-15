56bb87568d880120cb59e2da862f791763bd903b
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2024-01-14 21:21:19 -0500

    Revert "testing checkpoint system"
    This reverts commit 7c1efac605ec474cb2d36a6c3f01c0c84a05de78.

--- train.py
+++ train.py
@@ -23,8 +23,8 @@ print('label_size', label_size)
 
 batch_size = 18
 block_size = 1024
-checkpoint1_sec = 117
-total_train_sec = 234
+checkpoint1_sec = 11700
+total_train_sec = 23400
 num_embeddings = 512
 device = (
     'cuda'
