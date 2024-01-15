e274a21a862dc2183c9b9aad23fbb93a1cb10cda
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2024-01-14 21:45:23 -0500

    half the batch_size for 40gb a100

--- train.py
+++ train.py
@@ -23,7 +23,7 @@ labels_set_list = list(set(encoded_labels))
 label_size = len(labels_set_list)
 print('label_size', label_size)
 
-batch_size = 18
+batch_size = 9
 block_size = 1024
 checkpoint1_sec = 11700
 total_train_sec = 23400
