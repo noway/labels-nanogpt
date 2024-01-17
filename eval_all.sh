185b5605e790dd2896c92b34f41d7feab8c9d3d1
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2024-01-16 23:23:02 -0500

    add echos

--- eval_all.sh
+++ eval_all.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 set -x
-
+echo "------------------------------------------------------------"
 python generate_nshot_cot_exers.py && python eval_simple.py
 python generate_nshot_direct_exers.py && python eval_simple.py
 python generate_0shot_cot_exers.py && python eval_simple.py
 python generate_0shot_direct_exers.py && python eval_simple.py
 
+echo "------------------------------------------------------------"
 python generate_nshot_cot_exers.py && python eval_simple.py with_labels
 python generate_nshot_direct_exers.py && python eval_simple.py with_labels
 python generate_0shot_cot_exers.py && python eval_simple.py with_labels
 python generate_0shot_direct_exers.py && python eval_simple.py with_labels
 
+echo "------------------------------------------------------------"
 python generate_nshot_cot_exers.py && python eval.py
 python generate_nshot_direct_exers.py && python eval.py
 python generate_0shot_cot_exers.py && python eval.py
