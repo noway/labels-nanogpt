f2f0530d5115719a6636c8c95d8819d6d1994ba5
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2024-01-28 05:44:39 -0500

    half for no-label/label-embedding instead of double for label

--- train_simple.py
+++ train_simple.py
@@ -18,8 +18,8 @@ encoded = eval(json_str)
 chars = list(set(encoded))
 vocab_size = len(chars)
 
-batch_size = 4 if COMMONALITY_LABEL_ENABLED else 18
-block_size = 1024 * 2 if COMMONALITY_LABEL_ENABLED else 1024
+batch_size = 18 if COMMONALITY_LABEL_ENABLED else 18
+block_size = 1024 if COMMONALITY_LABEL_ENABLED else 512
 checkpoint1_sec = 11700
 total_train_sec = 23400
 num_embeddings = 512
