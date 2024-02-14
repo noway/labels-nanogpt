39ace73ee8798af2daf4aef10cb0b3984b6e61c4
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2024-02-13 23:58:24 -0500

    revert the previous commit - it was only needed to check once

--- eval_all.sh
+++ eval_all.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -x
-# echo "------------------------------------------------------------"
-# python generate_nshot_cot_exers.py && python eval_simple.py
-# python generate_nshot_direct_exers.py && python eval_simple.py
-# python generate_0shot_cot_exers.py && python eval_simple.py
-# python generate_0shot_direct_exers.py && python eval_simple.py
+echo "------------------------------------------------------------"
+python generate_nshot_cot_exers.py && python eval_simple.py
+python generate_nshot_direct_exers.py && python eval_simple.py
+python generate_0shot_cot_exers.py && python eval_simple.py
+python generate_0shot_direct_exers.py && python eval_simple.py
 
 echo "------------------------------------------------------------"
 python generate_nshot_cot_exers.py && python eval_simple.py with_labels
@@ -12,8 +12,8 @@ python generate_nshot_direct_exers.py && python eval_simple.py with_labels
 python generate_0shot_cot_exers.py && python eval_simple.py with_labels
 python generate_0shot_direct_exers.py && python eval_simple.py with_labels
 
-# echo "------------------------------------------------------------"
-# python generate_nshot_cot_exers.py && python eval.py
-# python generate_nshot_direct_exers.py && python eval.py
-# python generate_0shot_cot_exers.py && python eval.py
-# python generate_0shot_direct_exers.py && python eval.py
+echo "------------------------------------------------------------"
+python generate_nshot_cot_exers.py && python eval.py
+python generate_nshot_direct_exers.py && python eval.py
+python generate_0shot_cot_exers.py && python eval.py
+python generate_0shot_direct_exers.py && python eval.py
